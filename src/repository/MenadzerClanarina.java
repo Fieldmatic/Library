2da4895cd126e5fbe0b1cd4a77a7ef64f32b7160
fieldmatic <milicftn@gmail.com>
2021-07-02 01:18:31 +0200

    Dodat menadzer za clanarine, fajl za clanarine i popravljena klasa za clanarine

---
+++ src/repository/MenadzerClanarina.java
@@ -0,0 +1,43 @@
+package repository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import entities.Clanarina;
+import interfaces.menadzer;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MenadzerClanarina implements menadzer {
+    private List<Clanarina> clanarine = new ArrayList<>();
+    private static final String putanjaDoFajla = "fajlovi/Clanarine.json";
+
+    public MenadzerClanarina(){}
+
+    public void dodajClanarinu(Clanarina c) throws IOException {
+        clanarine.add(c);
+        azurirajFajl();
+    }
+
+
+    public void azurirajFajl() throws IOException {
+        ObjectMapper obj = new ObjectMapper();
+        FileWriter file = new FileWriter(putanjaDoFajla);
+        try{
+            String jsonStr = obj.writerWithDefaultPrettyPrinter().writeValueAsString(clanarine);
+            file.write(jsonStr);
+            file.close();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void ucitajPodatke() throws IOException {
+        ObjectMapper obj = new ObjectMapper();
+        clanarine = new ArrayList(Arrays.asList(obj.readValue(Paths.get(putanjaDoFajla).toFile(), Clanarina[].class)));
+    }
+}
