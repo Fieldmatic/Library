b74d8cfa5fe618770838f29c58c8e783735a9b01
ivana.ste <istevanovic3112@gmail.com>
2021-07-02 01:27:14 +0200

    Dodati fajlovi pozajmica i njihov menadzer

--- src/entities/Pozajmica.java
+++ src/entities/Pozajmica.java
@@ -1,15 +1,33 @@
 package entities;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import enumerations.StatusPozajmice;
+import localDateJson.LocalDateDeserializer;
+import localDateJson.LocalDateSerializer;
 
 import java.time.LocalDate;
 
 public class Pozajmica {
     private int id;
     private PrimerakKnjige pozajmljenPrimerak;
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate datumPocetka;
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate datumKraja;
     private StatusPozajmice status;
+
+    public Pozajmica() {}
+
+    public Pozajmica(int id, PrimerakKnjige pozajmljenPrimerak, LocalDate datumPocetka, LocalDate datumKraja, StatusPozajmice status) {
+        this.id = id;
+        this.pozajmljenPrimerak = pozajmljenPrimerak;
+        this.datumPocetka = datumPocetka;
+        this.datumKraja = datumKraja;
+        this.status = status;
+    }
 
     public int getId() {
         return id;
---
+++ src/repository/MenadzerPozajmica.java
@@ -0,0 +1,41 @@
+package repository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import entities.Pozajmica;
+import interfaces.menadzer;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MenadzerPozajmica implements menadzer {
+    private List<Pozajmica> pozajmice = new ArrayList<>();
+    private static final String putanjaDoFajla = "fajlovi/Pozajmice.json";
+
+    public MenadzerPozajmica() {}
+
+    public void dodajPozajmicu(Pozajmica pozajmica) throws IOException {
+        pozajmice.add(pozajmica);
+        azurirajFajl();
+    }
+
+    public void ucitajPodatke() throws IOException {
+        ObjectMapper obj = new ObjectMapper();
+        pozajmice = new ArrayList(Arrays.asList(obj.readValue(Paths.get(putanjaDoFajla).toFile(), Pozajmica[].class)));
+    }
+
+    public void azurirajFajl() throws IOException {
+        ObjectMapper obj = new ObjectMapper();
+        FileWriter file = new FileWriter(putanjaDoFajla);
+        try {
+            String jsonStr = obj.writerWithDefaultPrettyPrinter().writeValueAsString(pozajmice);
+            file.write(jsonStr);
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
